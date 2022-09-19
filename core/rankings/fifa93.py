61dcea4ef48489104617e31d74c6d71e6af2fcf2
ArkadiuszZajdel <arekzajdel.dom@gmail.com>
2022-09-19 21:20:32 +0200

    update structure fifa93

--- core/rankings/fifa93.py
+++ core/rankings/fifa93.py
@@ -4,20 +4,24 @@ from typing import Dict, Tuple
 from core.preprocessing import find_unique_countries
 from core.rankings.elo import calculate_elo_probability
 
-def estimate93(df: pd.DataFrame, importance: int, sensitivity: int, home_buff: int) -> Tuple[pd.DataFrame, Dict[str, int]]:
-    unique_countries = find_unique_countries(df)
-    
-    ranking = {country:0 for country in unique_countries}
+class FIFA93:
+    def __init__(self, df: pd.DataFrame, importance: int, sensitivity: int, home_buff: int):
+        self.df = df
+        self.importance = importance
+        self.sensitivity = sensitivity
+        self.home_buff = home_buff
 
-    df['We_home'] = df.apply(lambda row: _calculate_elo_probability_and_update_ranking(row.home_team, row.W_home, row.away_team, row.W_away, row.neutral, ranking, importance, sensitivity, home_buff), axis = 1)
-    df["W-We_home"] = df["W"]-df["We_home"] #nozliwe ze nie potrzebne bo to tylko do mse
-    return df, ranking
+        self.unique_countries = find_unique_countries(df)
+        self.ranking = {country:0 for country in self.unique_countries}
 
-def _calculate_elo_probability_and_update_ranking(country_home: str, score_home: int, 
-country_away: str, score_away: int,
-neutral: bool, ranking: Dict[str, int],
-importance: int, sensitivity: int, home_buff: int) -> float:
-    ranking[country_home] += score_home
-    ranking[country_away] += score_away
+    def estimate93(self) -> Tuple[pd.DataFrame, Dict[str, int]]:
+        self.df['We_home'] = self.df.apply(lambda row: self._calculate_elo_probability_and_update_ranking(row.home_team, row.W_home, row.away_team, row.W_away, row.neutral), axis = 1)
+        self.df["W-We_home"] = self.df["W"] - self.df["We_home"] #nozliwe ze nie potrzebne bo to tylko do mse
+        return self.df, self.ranking
 
-    return calculate_elo_probability(ranking[country_home], ranking[country_away], neutral, importance, sensitivity, home_buff)
+    def _calculate_elo_probability_and_update_ranking(self, country_home: str, score_home: int, 
+    country_away: str, score_away: int, neutral: bool) -> float:
+        self.ranking[country_home] += score_home
+        self.ranking[country_away] += score_away
+
+        return calculate_elo_probability(self.ranking[country_home], self.ranking[country_away], neutral, self.importance, self.sensitivity, self.home_buff)
